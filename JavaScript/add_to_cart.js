f4db42b77e53b8f8cb3221ace2fad89ff93a473b
Fabilane-meca <mecalynfabilane20@gmail.com>
2025-10-31 14:21:48 +0800

    Update add_to_cart.js

--- JavaScript/add_to_cart.js
+++ JavaScript/add_to_cart.js
@@ -206,10 +206,11 @@ document.addEventListener("DOMContentLoaded", () => {
       localStorage.setItem("checkoutCart", JSON.stringify(cartItems));
 
       // Redirect to checkout process page
-      window.location.href = 'checkout_process.html';
+      window.location.href = './checkout_process.html';
     });
   }
 });
+
 
 
 
